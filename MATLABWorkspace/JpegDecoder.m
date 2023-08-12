7cc44392b0684b4c85f1046dd52b8aa223fef2e7
JiachengMa <mjc020928@outlook.com>
2023-08-12 22:13:44 +0800

    correction: ACdecoder finished
    JpegDecoder to be dealt with

--- MATLABWorkspace/JpegDecoder.m
+++ MATLABWorkspace/JpegDecoder.m
@@ -2,5 +2,6 @@ function Image = JpegDecoder(DCstream,ACstream,img_h,img_w)
     blockamount = ceil(img_h/8)*ceil(img_w/8);
     DCarray = DCdecoder(DCstream,blockamount);
     ACarray = ACdecoder(ACstream,blockamount);
+    arrayFull = [DCarray;ACarray];
 end
 
