f3d77852f3c9c4100bf64ed48ebf05b04ce203ec
JiachengMa <mjc020928@outlook.com>
2023-09-07 17:36:23 +0800

    Done

--- MATLABWorkspace/FaceDetection.m
+++ MATLABWorkspace/FaceDetection.m
@@ -52,8 +52,8 @@ else
     v = v5;
 end
 
-plusStep = 10; % 向外延申的步长
-subStep = 7; %向内缩的步长，与前者互质，可以多次调整达到最优
+plusStep = 8; % 向外延申的步长
+subStep = 9; %向内缩的步长，与前者互质，可以多次调整达到最优
 for i = 1:size(margins,1) % refinement
     up = margins(i,1); % 上边框y值
     down = margins(i,2); % 下边框y值
@@ -62,89 +62,57 @@ for i = 1:size(margins,1) % refinement
     currDist = getFaceDist(testImg(up:down,left:right,:),v,L); %初步得到的人脸距离
 
     % up refinement
-    upNew = up; % 临时存储试探性调整后的新上边框y值
     distTmp = currDist; % distTmp存储试探性微调后当前的最优距离
-    upPlusDist = getFaceDist(testImg(max(upNew-plusStep,1):down,left:right,:),v,L); % 向外延申一个步长后的新距离
-    upSubDist = getFaceDist(testImg(min(upNew+subStep,down):down,left:right,:),v,L); % 向内缩一个步长后的新距离
-    minDistTmp = min([distTmp,upPlusDist,upSubDist]); % 当前距离、延申后距离和收缩后距离的最小值
+
+    upPlusDist = getFaceDist(testImg(max(up-plusStep,1):down,left:right,:),v,L); % 向外延申一个步长后的新距离
+    upSubDist = getFaceDist(testImg(min(up+subStep,down):down,left:right,:),v,L); % 向内缩一个步长后的新距离
+    downPlusDist = getFaceDist(testImg(up:min(down+plusStep,h),left:right,:),v,L);
+    downSubDist = getFaceDist(testImg(up:max(down-subStep,up),left:right,:),v,L);
+    leftPlusDist = getFaceDist(testImg(up:down,max(left-plusStep,1):right,:),v,L);
+    leftSubDist = getFaceDist(testImg(up:down,min(left+subStep,right):right,:),v,L);
+    rightPlusDist = getFaceDist(testImg(up:down,left:min(right+plusStep,w),:),v,L);
+    rightSubDist = getFaceDist(testImg(up:down,left:max(right-subStep,left),:),v,L);
+    minDistTmp = min([distTmp,upPlusDist,upSubDist,downPlusDist,downSubDist,leftPlusDist,leftSubDist,rightPlusDist,rightSubDist]); % 当前距离、延申后距离和收缩后距离的最小值
     while minDistTmp ~= distTmp % 如果当前并非最优
         if upPlusDist == minDistTmp % 如果向外伸展更优
             distTmp = upPlusDist; % 更新当前距离
-            upNew = upNew-plusStep; % 更新上边框y值
-        else % 如果向内缩更优
+            up = up-plusStep; % 更新上边框y值
+        elseif upSubDist == minDistTmp% 如果向内缩更优
             distTmp = upSubDist; % 更新当前距离
-            upNew = upNew+subStep; % 更新上边框y值
-        end
-        upPlusDist = getFaceDist(testImg(max(upNew-plusStep,1):down,left:right,:),v,L);
-        upSubDist = getFaceDist(testImg(min(upNew+subStep,down):down,left:right,:),v,L);
-        minDistTmp = min([distTmp,upPlusDist,upSubDist]);
-    end
-% 以下与对上边框的调整同理
-    % down refinement
-    downNew = down;
-    distTmp = currDist;
-    downPlusDist = getFaceDist(testImg(upNew:min(downNew+plusStep,h),left:right,:),v,L);
-    downSubDist = getFaceDist(testImg(upNew:max(downNew-subStep,upNew),left:right,:),v,L);
-    minDistTmp = min([distTmp,downPlusDist,downSubDist]);
-    while minDistTmp ~= distTmp
-        if downPlusDist == minDistTmp
+            up = up+subStep; % 更新上边框y值
+        elseif downPlusDist == minDistTmp
             distTmp = downPlusDist;
-            downNew = downNew+plusStep;
-        else
+            down = down + plusStep;
+        elseif downSubDist == minDistTmp
             distTmp = downSubDist;
-            downNew = downNew-subStep;
-        end
-        downPlusDist = getFaceDist(testImg(upNew:min(downNew+plusStep,h),left:right,:),v,L);
-        downSubDist = getFaceDist(testImg(upNew:max(downNew-subStep,upNew),left:right,:),v,L);
-        minDistTmp = min([distTmp,downPlusDist,downSubDist]);
-    end
-
-    margins(i,1) = upNew;
-    margins(i,2) = downNew;
-    up = upNew;
-    down = downNew;
-    currDist = getFaceDist(testImg(up:down,left:right,:),v,L);
-
-    % left refinement
-    leftNew = left;
-    distTmp = currDist;
-    leftPlusDist = getFaceDist(testImg(up:down,max(leftNew-plusStep,1):right,:),v,L);
-    leftSubDist = getFaceDist(testImg(up:down,min(leftNew+subStep,right):right,:),v,L);
-    minDistTmp = min([distTmp,leftPlusDist,leftSubDist]);
-    while minDistTmp ~= distTmp
-        if leftPlusDist == minDistTmp
+            down = down - subStep;
+        elseif leftPlusDist == minDistTmp
             distTmp = leftPlusDist;
-            leftNew = leftNew-plusStep;
-        else
+            left = left - plusStep;
+        elseif leftSubDist == minDistTmp
             distTmp = leftSubDist;
-            leftNew = leftNew+subStep;
-        end
-        leftPlusDist = getFaceDist(testImg(up:down,max(leftNew-plusStep,1):right,:),v,L);
-        leftSubDist = getFaceDist(testImg(up:down,min(leftNew+subStep,right):right,:),v,L);
-        minDistTmp = min([distTmp,leftPlusDist,leftSubDist]);
-    end
-
-    % right refinement
-    rightNew = right;
-    distTmp = currDist;
-    rightPlusDist = getFaceDist(testImg(up:down,leftNew:min(right+plusStep,w),:),v,L);
-    rightSubDist = getFaceDist(testImg(up:down,left:max(right-subStep,leftNew),:),v,L);
-    minDistTmp = min([distTmp,rightPlusDist,rightSubDist]);
-    while minDistTmp ~= distTmp
-        if rightPlusDist == minDistTmp
+            left = left + subStep;
+        elseif rightPlusDist == minDistTmp
             distTmp = rightPlusDist;
-            rightNew = rightNew+plusStep;
+            left = left + plusStep;
         else
             distTmp = rightSubDist;
-            rightNew = rightNew-subStep;
+            right = right - subStep;
         end
-        rightPlusDist = getFaceDist(testImg(up:down,leftNew:min(rightNew+plusStep,w),:),v,L);
-        rightSubDist = getFaceDist(testImg(up:down,leftNew:max(rightNew-subStep,leftNew),:),v,L);
-        minDistTmp = min([distTmp,rightPlusDist,rightSubDist]);
+        upPlusDist = getFaceDist(testImg(max(up-plusStep,1):down,left:right,:),v,L); % 向外延申一个步长后的新距离
+        upSubDist = getFaceDist(testImg(min(up+subStep,down):down,left:right,:),v,L); % 向内缩一个步长后的新距离
+        downPlusDist = getFaceDist(testImg(up:min(down+plusStep,h),left:right,:),v,L);
+        downSubDist = getFaceDist(testImg(up:max(down-subStep,up),left:right,:),v,L);
+        leftPlusDist = getFaceDist(testImg(up:down,max(left-plusStep,1):right,:),v,L);
+        leftSubDist = getFaceDist(testImg(up:down,min(left+subStep,right):right,:),v,L);
+        rightPlusDist = getFaceDist(testImg(up:down,left:min(right+plusStep,w),:),v,L);
+        rightSubDist = getFaceDist(testImg(up:down,left:max(right-subStep,left),:),v,L);
+        minDistTmp = min([distTmp,upPlusDist,upSubDist,downPlusDist,downSubDist,leftPlusDist,leftSubDist,rightPlusDist,rightSubDist]); % 当前距离、延申后距离和收缩后距离的最小值
     end
-    
-    margins(i,3) = leftNew;
-    margins(i,4) = rightNew;
+    margins(i,1) = up;
+    margins(i,2) = down;
+    margins(i,3) = left;
+    margins(i,4) = right;
 end
 end
 
